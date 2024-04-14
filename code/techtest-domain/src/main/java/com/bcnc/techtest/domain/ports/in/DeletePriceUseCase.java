1efe866d7ea758b208f39e6331a92f54fcf91ec6
Alejandro Muñoz Del Álamo <almudalamo@gmail.com>
2024-04-14 20:15:42 +0200

    feat(BCNCTT-4): Updated driver ports

--- code/techtest-domain/src/main/java/com/bcnc/techtest/domain/ports/in/DeletePriceUseCase.java
+++
@@ -1,7 +0,0 @@
-package com.bcnc.techtest.domain.ports.in;
-
-import java.time.LocalDateTime;
-
-public interface DeletePriceUseCase {
-    void deletePrice(int brandId, int product, LocalDateTime date);
-}
