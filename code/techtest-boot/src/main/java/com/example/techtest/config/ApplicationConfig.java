4fc62dbef7f1ecd99462c598ea359a0adf949c21
Alejandro Muñoz Del Álamo <almudalamo@gmail.com>
2024-04-20 21:03:17 +0200

    feat(BCNCTT-13):  Updated boot layer

--- code/techtest-boot/src/main/java/com/example/techtest/config/ApplicationConfig.java
+++ code/techtest-boot/src/main/java/com/example/techtest/config/ApplicationConfig.java
@@ -1,20 +1,17 @@
 package com.example.techtest.config;
 
 import jakarta.persistence.EntityManagerFactory;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.util.Map;
@@ -55,4 +52,5 @@ public class ApplicationConfig {
         transactionManager.setEntityManagerFactory(entityManagerFactory);
         return transactionManager;
     }
+
 }
